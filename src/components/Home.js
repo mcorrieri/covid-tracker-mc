8a1d308bfa5672c317bdcbf29be7626e9b9556d3
Michael <Mlcorrieri@gmail.com>
2021-11-07 15:29:03 -0500

    covid data fetched

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,26 +2,41 @@ import React, { useState, useEffect } from "react";
 import { Button } from "@mui/material";
 
 function Home() {
-  const [current, setCurrent] = useState([]);
+  const [currentData, setCurrentData] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetchFoxes();
+    fetchData();
   }, []);
 
-  async function fetchFoxes() {
-    const response = await fetch("https://randomfox.ca/floof/")
+  // async function fetchFoxes() {
+  //   const response = await fetch("https://randomfox.ca/floof/")
+  //     .then((res) => res.json())
+  //     .then((data) => setCurrentData(data));
+  // }
+
+  async function fetchData() {
+    const response = await fetch(
+      "https://disease.sh/v3/covid-19/nyt/states?lastdays=1"
+    )
       .then((res) => res.json())
-      .then((data) => setCurrent(data));
+      .then((data) => setCurrentData(data));
   }
+  console.log(currentData);
+
+  const countryList = currentData.map((item) => item.state);
+
+  console.log(countryList);
 
   return (
     <div className="home">
       <h2 className="header">COVID TRACKER</h2>
       <p>HOME</p>
-      <img src={current.image} alt="fox" />
+      <ul>
+        <li>{countryList}</li>
+      </ul>
       <br></br>
-      <Button onClick={fetchFoxes} variant="outlined">
+      <Button onClick={fetchData} variant="outlined">
         Get a new fox!
       </Button>
     </div>
