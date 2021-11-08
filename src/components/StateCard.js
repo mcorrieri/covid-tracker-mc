9cc3936f23ff55eb80d9d960118adde9a76b8637
Michael <Mlcorrieri@gmail.com>
2021-11-07 22:32:01 -0500

    basic styling state cards

--- src/components/StateCard.js
+++ src/components/StateCard.js
@@ -1,18 +1,23 @@
 import React from "react";
 import Card from "@mui/material/Card";
-import { CardContent, Typography } from "@mui/material";
+import { Button, CardActions, CardContent, Typography } from "@mui/material";
 
 function StateList({ item }) {
   const { state, cases, date, deaths } = item;
   return (
     <div>
-      <Card variant="outlined">
+      <Card sx={{ maxWidth: 275, backgroundColor: "grey" }} variant="outlined">
         <CardContent>
-          <Typography sx={{ fontSize: 20 }}>{state}</Typography>
+          <Typography color="GrayText" sx={{ fontSize: 20 }}>
+            {state}
+          </Typography>
           <Typography>Date: {date}</Typography>
           <Typography>Cases: {cases}</Typography>
           <Typography>Deaths: {deaths}</Typography>
         </CardContent>
+        <CardActions>
+          <Button size="small">See History</Button>
+        </CardActions>
       </Card>
     </div>
   );
